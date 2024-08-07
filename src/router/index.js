8d622c6b95576b6c52f6dd82d8f0af81222d1f94
ramiro.santos <ramiro.santos@cardinalsisa.com>
2024-08-06 23:54:19 -0300

    Agrega vista y router para LoginRegistration

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginRegistrationView from '../views/LoginRegistrationView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'loginregistration',
+    component: LoginRegistrationView
   },
   {
     path: '/about',
